203f9a58ee9f2adf95a9f9caf97bcbfd74ed4fb7
Cody Durham <codydd@gmail.com>
2021-07-29 18:58:02 -0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './Pages/Main'
+import About from './Pages/About'
+import Projects from './Pages/Projects';
+import Designer from './Pages/Designer';
+import Contact from './Pages/Contact';
+import Resume from './Pages/Resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Main /> 
+      <About /> 
+      <Projects />
+      <Designer />
+      <Contact /> 
+      <Resume /> 
+   
     </div>
   );
 }
