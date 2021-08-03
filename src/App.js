3313a9e7e225801d388fd14f45df7a79813f67ad
Cody Durham <codydd@gmail.com>
2021-08-02 23:56:36 -0600

    Trying to get CORS error fixed. Multiple trys, not complete

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ import Resume from './Pages/Resume'
 import Footer from './Components/Footer';
 import './Styles/App.scss'
 import {Switch, Route, Link} from 'react-router-dom'
-
+import {useState} from 'react'
 
 
 
 
 function App() {
+  const googleURL = 'https://docs.google.com/spreadsheets/d/1j85QSABivRQO2ZJQvY48sfWTUv_2jky5JM7abbd6duo/edit#gid=0'
+    // console.log('google spreadsheet data =>',googleURL);
+    // const split = googleURL.entry[0].gsx$project.$t
+ 
   return (
     <div className="App">
       <Header /> 
 
         <Switch>
           <Route exact path='/'>
-            <Main /> 
+            <Main googleURL={ googleURL } /> 
           </Route>
           
           <Route path='/about' component= { About } >
