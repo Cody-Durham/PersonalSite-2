86502825f4025dbe051dea9a62b2e9c8fd006f95
Cody Durham <codydd@gmail.com>
2021-08-07 22:05:21 -0600

    Add more lego images for randomizing

--- src/Components/UnderConstruction.jsx
+++ src/Components/UnderConstruction.jsx
@@ -18,7 +18,16 @@ const constructionImages = [
     'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628394325/Portfolio%20Website%202/alan-hardman-Y-hl3rx5fr8-unsplash_evpzso.jpg',
     'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628394358/Portfolio%20Website%202/zakaria-ahada-0xOCVe7nUU0-unsplash_oxkmue.jpg', 
     'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628394384/Portfolio%20Website%202/daniel-cheung-p-HOSqp1Ms4-unsplash_s2onze.jpg',
-
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395152/Portfolio%20Website%202/alan-hardman-2epxSXR-e68-unsplash_v6fjax.jpg', 
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395178/Portfolio%20Website%202/marton-szalai-Kzh_MJof2-c-unsplash_wwyoot.jpg', 
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395208/Portfolio%20Website%202/daniel-cheung-mvo-xJE1oFg-unsplash_jrnwgv.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395228/Portfolio%20Website%202/hello-i-m-nik-fDaUCTp28dA-unsplash_xyyu13.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395272/Portfolio%20Website%202/phil-hearing-PhnJhjH9Y9s-unsplash_vemkhm.jpg', 
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395298/Portfolio%20Website%202/hello-i-m-nik-2bVe_GVW7rw-unsplash_d361cy.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395331/Portfolio%20Website%202/daniel-cheung--oM_twQDw2Y-unsplash_smdlch.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395361/Portfolio%20Website%202/daniel-cheung-zky0BmMT5m8-unsplash_pryzps.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395399/Portfolio%20Website%202/daniel-cheung-B7N0IjiIJYo-unsplash_ywrvqq.jpg',
+    'https://res.cloudinary.com/dhad6e9gj/image/upload/v1628395430/Portfolio%20Website%202/hello-i-m-nik-l4ADb9OVqTY-unsplash_vtycoq.jpg',
 ]
 
 
