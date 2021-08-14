e889b63b26141a4162d798fca5f6a83870fd1a44
codydurham <codydd@gmail.com>
2021-08-13 19:31:58 -0600

    Add "software developer" img to header with styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
             <div className='header-container'>
                 <div className='logo'>
                     <Link to='/'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1627624646/Portfolio%20Website%202/CD_Logo_White-02_m9ldd1.png' alt='my custom logo'/></Link>
+                    <img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1628904616/Portfolio%20Website%202/SoftwareDeveloper_White_Icons-34_futaru.png' className=' software-developer'></img>
                 </div>
                 <nav>
                     <ul>
